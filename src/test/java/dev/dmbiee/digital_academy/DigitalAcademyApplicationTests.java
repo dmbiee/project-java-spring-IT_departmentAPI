6fa20e49f13cc0bda269ec381b82ff5ac0ed8385
Sofia Belei <beleyhha@gmail.com>
2025-09-02 11:01:13 +0200

    init: initializar repository

---
+++ src/test/java/dev/dmbiee/digital_academy/DigitalAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dmbiee.digital_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
