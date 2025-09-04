c48dce099cb9a32eef2d0de43126ca79b95606d1
Dmytro Belei <beleyhha@gmail.com>
2025-09-04 09:11:12 +0200

    feat: add user entity and table in db

---
+++ src/main/java/dev/dmbiee/digital_academy/user/UserEntity.java
@@ -0,0 +1,62 @@
+package dev.dmbiee.digital_academy.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long user_id;
+    private String name;
+    private String surname;
+    private long role_id;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(long user_id, String name, String surname, long role_id) {
+        this.user_id = user_id;
+        this.name = name;
+        this.surname = surname;
+        this.role_id = role_id;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public long getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(long role_id) {
+        this.role_id = role_id;
+    }
+
+}
